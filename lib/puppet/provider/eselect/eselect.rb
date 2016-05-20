whatbox/PortageGT

commit 56a397e335b667ca4ecf1d72e1f0f4bc2b827046

Minor code cleanup

lib/puppet/provider/eselect/eselect.rb
@@ -29,9 +29,7 @@ Puppet::Type.type(:eselect).provide(:eselect) do
     end
 
     if @resource[:module]
-      if name_module && emodule != @resource[:module]
-        raise Puppet::Error, "Module disagreement on eselect[#{name}], please check the definition"
-      end
+      raise Puppet::Error, "Module disagreement on eselect[#{name}], please check the definition" if name_module && emodule != @resource[:module]
 
       emodule = @resource[:module]
     end
@@ -51,17 +49,9 @@ Puppet::Type.type(:eselect).provide(:eselect) do
   # string (void)
   def eselect_list
     if @resource[:listcmd]
-      if @resource[:module]
-        raise Puppet::Error, "module should not be specified if listcmd is in eselect[#{@resource[:name]}], please check the definition"
-      end
-
-      if @resource[:submodule]
-        raise Puppet::Error, "submodule should not be specified if listcmd is in eselect[#{@resource[:name]}], please check the definition"
-      end
-
-      if @resource[:setcmd].nil?
-        raise Puppet::Error, "listcmd is specified but setcmd is not in eselect[#{@resource[:name]}], please check the definition"
-      end
+      raise Puppet::Error, "module should not be specified if listcmd is in eselect[#{@resource[:name]}], please check the definition" if @resource[:module]
+      raise Puppet::Error, "submodule should not be specified if listcmd is in eselect[#{@resource[:name]}], please check the definition" if @resource[:submodule]
+      raise Puppet::Error, "listcmd is specified but setcmd is not in eselect[#{@resource[:name]}], please check the definition" if @resource[:setcmd].nil?
 
       return @resource[:listcmd]
     end
@@ -76,17 +66,9 @@ Puppet::Type.type(:eselect).provide(:eselect) do
   # string (void)
   def eselect_set
     if @resource[:setcmd]
-      if @resource[:module]
-        raise Puppet::Error, "module should not be specified if setcmd is in eselect[#{@resource[:name]}], please check the definition"
-      end
-
-      if @resource[:submodule]
-        raise Puppet::Error, "submodule should not be specified if setcmd is in eselect[#{@resource[:name]}], please check the definition"
-      end
-
-      if @resource[:listcmd].nil?
-        raise Puppet::Error, "setcmd is specified but listcmd is not in eselect[#{@resource[:name]}], please check the definition"
-      end
+      raise Puppet::Error, "module should not be specified if setcmd is in eselect[#{@resource[:name]}], please check the definition" if @resource[:module]
+      raise Puppet::Error, "submodule should not be specified if setcmd is in eselect[#{@resource[:name]}], please check the definition" if @resource[:submodule]
+      raise Puppet::Error, "setcmd is specified but listcmd is not in eselect[#{@resource[:name]}], please check the definition" if @resource[:listcmd].nil?
 
       return @resource[:setcmd]
     end
@@ -111,19 +93,14 @@ Puppet::Type.type(:eselect).provide(:eselect) do
 
       next if option[0] !~ /\[\d+\]/
 
-      if option[option.length - 1] == '*'
+      if option.end_with('*')
         raise Puppet::Error, "Multiple selected versions for eselect[#{@resource[:name]}]" unless selected.nil?
-
-        selected = option[1]
       end
 
       options.push(option[1])
     end
 
-    unless options.include? should
-      available_options = options.join(' ')
-      raise Puppet::Error, "Invalid option \"#{should}\", should be one of [#{available_options}] for eselect[#{@resource[:name]}]"
-    end
+    should raise Puppet::Error, "Invalid option \"#{should}\", should be one of [#{options.join(' ')}] for eselect[#{@resource[:name]}]" unless options.include?
 
     selected
   end

lib/puppet/provider/package/portagegt.rb
@@ -107,14 +107,12 @@ Puppet::Type.type(:package).provide(
   # void (void)
   def self.run_eix
     unless EIX_RUN_UPDATE
-      if EIX_RUN_SYNC >= 0
-        raise Puppet::Error, 'EIX_RUN_UPDATE must be true if EIX_RUN_SYNC is not -1'
-      end
+      raise Puppet::Error, 'EIX_RUN_UPDATE must be true if EIX_RUN_SYNC is not -1' if EIX_RUN_SYNC >= 0
       return
     end
 
-    if EIX_RUN_SYNC >= 0
-      eix_sync if File.mtime(TIMESTAMP_FILE) + EIX_RUN_SYNC < Time.now
+    if EIX_RUN_SYNC >= 0 && File.mtime(TIMESTAMP_FILE) + EIX_RUN_SYNC < Time.now
+      eix_sync
     else
       eix_update
     end
@@ -137,9 +135,7 @@ Puppet::Type.type(:package).provide(
 
       # We cannot specify these attributes unless we have a category as well
       if category.nil?
-        unless opt_flags.empty?
-          Puppet.warning("Cannot apply #{function} for Package[#{name}] without a category")
-        end
+        Puppet.warning("Cannot apply #{function} for Package[#{name}] without a category") unless opt_flags.empty?
         next
       end
 
@@ -324,9 +320,7 @@ Puppet::Type.type(:package).provide(
     end
 
     if @resource[:category]
-      if name_category && category != @resource[:category]
-        raise Puppet::Error, "Category disagreement on Package[#{name}]"
-      end
+      raise Puppet::Error, "Category disagreement on Package[#{name}]" if name_category && category != @resource[:category]
 
       category = @resource[:category]
     end
@@ -348,9 +342,7 @@ Puppet::Type.type(:package).provide(
 
     unless @resource[:package_settings].nil?
       if @resource[:package_settings].key?('slot')
-        if name_slot && slot != @resource[:package_settings]['slot'].to_s
-          raise Puppet::Error, "Slot disagreement on Package[#{name}]"
-        end
+        raise Puppet::Error, "Slot disagreement on Package[#{name}]" if name_slot && slot != @resource[:package_settings]['slot'].to_s
 
         slot = @resource[:package_settings]['slot']
       end
@@ -486,9 +478,7 @@ Puppet::Type.type(:package).provide(
 
     _package_glob.each do |directory|
       %w(SLOT PF CATEGORY repository USE).each do |expected|
-        unless File.exist?("#{directory}/#{expected}")
-          raise Puppet::Error, "The metadata file \"#{expected}\" was not found in #{directory}"
-        end
+        raise Puppet::Error, "The metadata file \"#{expected}\" was not found in #{directory}" unless File.exist?("#{directory}/#{expected}")
       end
 
       slot = _strip_subslot(File.read("#{directory}/SLOT").rstrip)
@@ -525,13 +515,8 @@ Puppet::Type.type(:package).provide(
     end
 
     # Disambiguation errors
-    if categories.length > 1
-      raise Puppet::Error, "Package[#{@resource[:name]}] is ambiguous, specify a category: #{categories.to_a.join(', ')}"
-    end
-
-    if slots.length > 1
-      raise Puppet::Error, "Package[#{@resource[:name]}] is ambiguous, specify a slot: #{slots.keys.join(', ')}"
-    end
+    raise Puppet::Error, "Package[#{@resource[:name]}] is ambiguous, specify a category: #{categories.to_a.join(', ')}" if categories.length > 1
+    raise Puppet::Error, "Package[#{@resource[:name]}] is ambiguous, specify a slot: #{slots.keys.join(', ')}" if slots.length > 1
 
     slots.values.first
   end

commit a40bfe73258aad389a3196552fe668718207d157

Reorganize tests for eselect and fix a regression introduced by the cleanup in 56a397e335b667ca4ecf1d72e1f0f4bc2b827046 that caused broken eselect functionality.

lib/puppet/provider/eselect/eselect.rb
@@ -93,7 +93,7 @@ Puppet::Type.type(:eselect).provide(:eselect) do
 
       next if option[0] !~ /\[\d+\]/
 
-      if option.end_with('*')
+      if option.last == '*'
         raise Puppet::Error, "Multiple selected versions for eselect[#{@resource[:name]}]" unless selected.nil?
       end
 